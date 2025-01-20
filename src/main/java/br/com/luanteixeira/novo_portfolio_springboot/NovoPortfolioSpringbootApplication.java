65e88529bca8c0a632375892fe0ae091b87aace9
Luan Teixeira <luanlts@outlook.com>
2025-01-20 15:09:22 -0300

    initial commit

---
+++ src/main/java/br/com/luanteixeira/novo_portfolio_springboot/NovoPortfolioSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.luanteixeira.novo_portfolio_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovoPortfolioSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovoPortfolioSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luanteixeira/novo_portfolio_springboot/NovoPortfolioSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luanteixeira.novo_portfolio_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovoPortfolioSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
